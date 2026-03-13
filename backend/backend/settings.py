c5c7ac995b70550e5eb21f41047afafb275a499f
杨少 <ychvad@163.com>
2026-03-14 01:38:33 +0800

    后端增加https

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,6 +18,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Trust proxy headers (Caddy -> FRP -> Django)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
